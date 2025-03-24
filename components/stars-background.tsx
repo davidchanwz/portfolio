bcf30b59b4a68bf6592ca3100ffe8d279fb9f63d
David Chan <davidchanwz@gmail.com>
2025-03-25 00:40:12 +0800

    fix stars on mobile 2

--- components/stars-background.tsx
+++ components/stars-background.tsx
@@ -55,7 +55,7 @@ function Stars() {
 
 export default function StarsBackground() {
     return (
-        <div className="fixed top-0 left-0 w-full h-screen -z-10 overflow-hidden">
+        <div className="fixed top-0 left-0 w-full min-h-screen -z-10 overflow-hidden touch-action-none">
             <Canvas camera={{ position: [0, 0, 1] }}>
                 <Stars />
             </Canvas>
